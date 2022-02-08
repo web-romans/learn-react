2a40c4a758ca9b792eb1667b0793453fd6ebea0c
Web-romans <roma88gruzd88@mail.ru>
2022-02-08 16:28:50 +0300

    lesson 24 UI - BLL

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -4,10 +4,25 @@ import Message from './Message/Message';
 import './Dialogs.scss'
 
 function Dialogs(props) {
+
+    let dialogsData = [
+        { id: '1', name: 'Роман' },
+        { id: '2', name: 'Татьяна' },
+        { id: '3', name: 'Анна' },
+        { id: '4', name: 'Иван' },
+    ];
+
+    let messagesData = [
+        { id: '0', message: 'Привет!' },
+        { id: '1', message: 'Как дела?' },
+        { id: '2', message: 'Хорошо' },
+        { id: '3', message: 'Как там реакт?' },
+    ];
+
     return (
         <div className='dialogs'>
             <div className="dialogs__items">
-                <Dialog id="1" name="Роман" />
+                <Dialog id={dialogsData[0].id} name={dialogsData[0].name} />
                 <Dialog id="2" name="Татьяна" />
                 <Dialog id="3" name="Анна" />
                 <Dialog id="4" name="Иван" />
@@ -16,15 +31,12 @@ function Dialogs(props) {
             <div className="dialogs__messages messages">
 
                 <div className="messages__items">
-                    <Message message="Привет!" />
-                    <Message message="Привет!" />
+                    <Message message={messagesData[0].message} />
+                    <Message message={messagesData[1].message} />
+                    <Message message={messagesData[2].message} />
+                    <Message message={messagesData[3].message} />
 
-                    <div className="messages__item message message--received">
-                        Я ок!
-                    </div>
-                    <div className="messages__item message message--received">
-                        Как там react?
-                    </div>
+                    {/*<div className="messages__item message message--received">*/}
                 </div>
 
 
--- src/components/Proffile/Posts/Posts.js
+++ src/components/Proffile/Posts/Posts.js
@@ -2,6 +2,36 @@ import Post from './Post/Post';
 
 import './Posts.scss';
 function Posts() {
+
+    let postData = [
+        {
+            id: '0',
+            img: 'https://dineuron.com/uploads/services/service_5cac7af9a951513.png',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut culpa doloremque similique nam eum suscipit, atque magni provident vero assumenda! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo nulla cum facilis reprehenderit illo dolore totam? Mollitia incidunt omnis consequuntur impedit quod aspernatur accusantium nihil ullam illo dignissimos aliquam laboriosam facere eum amet similique animi quibusdam nesciunt, in neque dicta!',
+            likes: '3'
+        },
+        {
+            id: '1',
+            img: 'https://dineuron.com/uploads/services/service_5cac7af9a951513.png',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum! Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            likes: '2'
+        },
+        {
+            id: '2',
+            img: 'https://dineuron.com/uploads/services/service_5cac7af9a951513.png',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo nulla cum facilis reprehenderit illo dolore totam? Mollitia incidunt omnis consequuntur impedit quod aspernatur accusantium nihil ullam illo dignissimos aliquam laboriosam facere eum amet similique animi quibusdam nesciunt, in neque dicta!',
+            likes: '1'
+        },
+        {
+            id: '3',
+            img: 'https://dineuron.com/uploads/services/service_5cac7af9a951513.png',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+            likes: '24'
+        },
+
+    ];
+
+
     return (
         <div className="proffile__posts posts">
             <form className="posts__form form">
@@ -10,19 +40,14 @@ function Posts() {
             </form>
 
             <Post
-                img='https://dineuron.com/uploads/services/service_5cac7af9a951513.png'
-                text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut culpa doloremque similique nam eum suscipit, atque magni provident vero assumenda! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo nulla cum facilis reprehenderit illo dolore totam? Mollitia incidunt omnis consequuntur impedit quod aspernatur accusantium nihil ullam illo dignissimos aliquam laboriosam facere eum amet similique animi quibusdam nesciunt, in neque dicta!'
-                likes='3'
+                img={postData[0].img}
+                text={postData[0].text}
+                likes={postData[0].likes}
             />
             <Post
-                img='https://dineuron.com/uploads/services/service_5cac7af9a951513.png'
-                text='Lorem ipsum dolor sit amet consectetur adipisicing elit. '
-                likes='2'
-            />
-            <Post
-                img='https://dineuron.com/uploads/services/service_5cac7af9a951513.png'
-                text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum! Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
-                likes='1'
+                img={postData[1].img}
+                text={postData[1].text}
+                likes={postData[1].likes}
             />
         </div>
     )
